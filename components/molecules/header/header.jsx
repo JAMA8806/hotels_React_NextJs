a1e65f548b8e4970432e1c5466d34dd744dddae5
JAMA8806 <julianb.e8806@gmail.com>
2023-10-04 15:36:48 -0500

    Header.jsx was adjusted accordingly with teacher comments

--- components/molecules/header/header.jsx
+++ components/molecules/header/header.jsx
@@ -1,5 +1,3 @@
-
-
 import styles from './header.module.css'
 
 export const Header = ()=> {
@@ -14,18 +12,14 @@ export const Header = ()=> {
                     <option value="chile">Chile</option>
                     <option value="uruguay">Uruguay</option>
                 </select>
-
-
                 <input type="date" className={`${styles.input} ${styles.filtersBox__input}`} id="" />
                 <input type="date" className={`${styles.input} ${styles.filtersBox__input}`} id="" />
-
                 <select name="" id="" className={`${styles.filtersBox__input} ${styles.input}`}>
                     <option value="All">All sizes</option>
                     <option value="small">Small</option>
                     <option value="medium">Medium</option>
                     <option value="large">Large</option>                    
                 </select>
-
                 <select name="" id="" className={`${styles.filtersBox__input} ${styles.input}`}>
                     <option value="All">All prices</option>
                     <option value="$">$</option>
