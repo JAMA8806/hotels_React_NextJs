371ff56a0d32746f1a8de78e341aed55ac6302e7
JAMA8806 <julianb.e8806@gmail.com>
2023-10-06 17:46:24 -0500

    some styles were adjusted

--- components/organisms/cardsFilter/hotelsReservations/hotelsReservations.jsx
+++ components/organisms/cardsFilter/hotelsReservations/hotelsReservations.jsx
@@ -2,16 +2,16 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 import MediaCard from '../../../molecules/card/card';
-
+import styles from './hotelsReservations.module.css'
 export const HotelsReservations = () => {
     const hotelsReservationList = useSelector(
         (state) => state.reservation.hotelsReservation
       );
-      console.log(hotelsReservationList);
+      
   return (
-    <div>
+    <div className={styles.cardsContainer}>
         {hotelsReservationList.map((hotel,index)=>{
-            console.log(hotel.payload);
+            
             return (
                 <MediaCard key={index} hotel={hotel.payload}>Cards</MediaCard>
             )
--- src/app/reservations/page.js
+++ src/app/reservations/page.js
@@ -1,16 +1,16 @@
-"use client"
-import React from 'react'
-import { ReservationsTemplate } from '../../../components/templates/cardsFilter-template/reservationsTemplate/reservationsTemplate'
-
+"use client";
+import React from "react";
+import { ReservationsTemplate } from "../../../components/templates/cardsFilter-template/reservationsTemplate/reservationsTemplate";
+import styles from "./page.module.css";
 const Reservations = () => {
   return (
-    <div>
-        <h1>
-            Hotels reservations
-        </h1>
+    <div className={styles.reservationsContainer}>
+      <h1 className={styles.h1}>Hotels reservations</h1>
+      <div>
         <ReservationsTemplate></ReservationsTemplate>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Reservations;
